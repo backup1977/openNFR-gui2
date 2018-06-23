7cf006676f26ddf799451066edca25ce9087775b
backup1977 <waelghani@yahoo.de>
2018-06-23 16:25:55 +0100

    Update skin.py

--- skin.py
+++ skin.py
@@ -104,9 +104,9 @@ config.skin = ConfigSubsection()
 config.defaultskinSetup = ConfigSubsection()
 config.defaultskinSetup.steps = ConfigSelection([('default Utopia',_("default Utopia")),('default SmokeR',_("default SmokeR"))])
 if config.defaultskinSetup.steps.value == "default Utopia":
-       	DEFAULT_SKIN = "Blue-Line-OCT-4NFR/skin.xml"
+       	DEFAULT_SKIN = "iFlatFHD/skin.xml"
 elif config.defaultskinSetup.steps.value == "default SmokeR":
-       	DEFAULT_SKIN = "skin_default/skin.xml"        
+       	DEFAULT_SKIN = "iFlatFHD/skin.xml"        
 
 if not fileExists(resolveFilename(SCOPE_SKIN, DEFAULT_SKIN)):
 	# in that case, fallback to Magic (which is an SD skin)
